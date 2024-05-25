780ad44f224ee41d0b4be006eb36524c0b08e598
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-25 10:29:38 +0330

    Update iPmart.sh

--- iPmart.sh
+++ iPmart.sh
@@ -632,7 +632,7 @@ download_and_extract_rathole() {
         exit 1
     fi
     if [[ "$ARCH" == "x86_64" ]]; then
-    	DOWNLOAD_URL='https://github.com/ipmartnetwork/RatholeTunnel/raw/main/rathole.zip'
+    	DOWNLOAD_URL=$(curl -sSL https://api.github.com/repos/ipmartnetwork/rathole/releases/latest | grep -o "https://.*$ARCH.*linux.*zip" | head -n 1)
     fi
 
     if [ -z "$DOWNLOAD_URL" ]; then
