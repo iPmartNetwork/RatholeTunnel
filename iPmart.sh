2aae9ff07d35677c40a0e19b459f6e3527e9f8bf
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-26 15:29:54 +0330

    Update iPmart.sh

--- iPmart.sh
+++ iPmart.sh
@@ -632,7 +632,7 @@ download_and_extract_rathole() {
         exit 1
     fi
     if [[ "$ARCH" == "x86_64" ]]; then
-    	DOWNLOAD_URL='https://github.com/iPmartNetwork/RatholeTunnel/releases/download/v0.5.0/rathole-arm-unknown-linux-musleabi.zip'
+    	DOWNLOAD_URL='https://github.com/iPmartNetwork/RatholeTunnel/releases/download/v0.5.0/rathole-x86_64-unknown-linux-gnu.zip'
     fi
 
     if [ -z "$DOWNLOAD_URL" ]; then
